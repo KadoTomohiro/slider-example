bd4fd9b19f8d3dd59b88700fa15fb754378dc23f
KadoTomohiro <>
2022-02-17 15:13:11 +0900

    create sample

---
+++ components/vuetify-carousel.vue
@@ -0,0 +1,42 @@
+<template>
+  <!-- これがないとスタイル崩れる。逆に、これを閉じ込めておけばスタイル漏洩しないかも -->
+  <v-app>
+    <!--  サイズをこれで制御。width, heightはpropsにしてバインド  -->
+    <v-sheet width="800" height="500">
+      <!-- カルーセルここから。細かい設定はDocument参照-->
+      <v-carousel height="auto">
+        <v-carousel-item v-for="(item, i) in items" :key="i" :src="item.src"></v-carousel-item>
+      </v-carousel>
+    </v-sheet>
+  </v-app>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'VuetifyCarousel',
+  data() {
+    return {
+      items: [
+        {
+          src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
+        },
+        {
+          src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
+        },
+        {
+          src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
+        },
+        {
+          src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
+        },
+      ],
+    }
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
